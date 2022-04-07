cee640ab499918de6116e410ec32b8e34798a1b6
William Baldoumas <william.baldoumas@gmail.com>
2022-04-06 20:27:13 -0400

    Initial commit

---
+++ src/EldenRingOptimizer.Engine/Records/JsonArmorPiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EldenRingOptimizer.Engine.Records;
+
+public record JsonArmorPiece(
+    [property: JsonPropertyName("name")] string? Name,
+    [property: JsonPropertyName("type")] string Type,
+    [property: JsonPropertyName("weight")] double? Weight,
+    [property: JsonPropertyName("physical")] double? Physical,
+    [property: JsonPropertyName("strike")] double? Strike,
+    [property: JsonPropertyName("slash")] double? Slash,
+    [property: JsonPropertyName("pierce")] double? Pierce,
+    [property: JsonPropertyName("magic")] double? Magic,
+    [property: JsonPropertyName("fire")] double? Fire,
+    [property: JsonPropertyName("lightning")] double? Lightning,
+    [property: JsonPropertyName("holy")] double? Holy,
+    [property: JsonPropertyName("immunity")] double? Immunity,
+    [property: JsonPropertyName("robustness")] double? Robustness,
+    [property: JsonPropertyName("focus")] double? Focus,
+    [property: JsonPropertyName("vitality")] double? Vitality,
+    [property: JsonPropertyName("poise")] double? Poise
+);
