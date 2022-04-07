a809bd73aa743dac295320bfb8c670f79a1561be
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2022-04-06 23:07:18 -0400

    Other repositories (#5)

---
+++ src/EldenRingOptimizer.Engine/Repositories/ITalismanRepository.cs
@@ -0,0 +1,15 @@
+﻿using EldenRingOptimizer.Engine.Records;
+
+namespace EldenRingOptimizer.Engine.Repositories;
+
+/// <summary>
+///     A repository of <see cref="Talisman"/>.
+/// </summary>
+public interface ITalismanRepository
+{
+    /// <summary>
+    ///     Retrieve all of the talismans.
+    /// </summary>
+    /// <returns>All of the talismans.</returns>
+    Task<IEnumerable<Talisman>> GetAll();
+}
---
+++ src/EldenRingOptimizer.Engine/Repositories/IWeaponRepository.cs
@@ -0,0 +1,15 @@
+﻿using EldenRingOptimizer.Engine.Records;
+
+namespace EldenRingOptimizer.Engine.Repositories;
+
+/// <summary>
+///     A repository of <see cref="Weapon"/>.
+/// </summary>
+public interface IWeaponRepository
+{
+    /// <summary>
+    ///     Retrieve all of the weapons.
+    /// </summary>
+    /// <returns>All of the weapons.</returns>
+    Task<IEnumerable<Weapon>> GetAll();
+}
