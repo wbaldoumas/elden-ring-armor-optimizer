c619dc0c5555d68ad36a007d504bda259980ae8e
Will Baldoumas <45316999+wbaldoumas@users.noreply.github.com>
2022-04-10 17:08:24 -0400

    Additional Test Coverage (#18)

---
+++ tests/EldenRingArmorOptimizer.Tests/Repositories/MockHttpMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+
+namespace EldenRingArmorOptimizer.Tests.Repositories;
+
+[ExcludeFromCodeCoverage]
+public class MockHttpMessageHandler : HttpMessageHandler
+{
+    private readonly string _response;
+
+    public MockHttpMessageHandler(string response)
+    {
+        _response = response;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        return await Task.FromResult(
+            new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(_response)
+            }
+        );
+    }
+}
---
+++ tests/EldenRingArmorOptimizer.Tests/Repositories/WeaponRepositoryTests.cs
@@ -0,0 +1,43 @@
+﻿using EldenRingArmorOptimizer.Engine.Configuration;
+using EldenRingArmorOptimizer.Engine.Records;
+using EldenRingArmorOptimizer.Engine.Repositories;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace EldenRingArmorOptimizer.Tests.Repositories;
+
+[TestFixture]
+public class WeaponRepositoryTests
+{
+    [Test]
+    public async Task WeaponRepository_returns_expected_weapons()
+    {
+        // arrange
+        const string response = "[{\"Name\":\"Hand Axe\",\"Weight\":3.5},{\"Name\":\"Forked Hatchet\",\"Weight\":2.5},{\"Name\":\"Battle Axe\",\"Weight\":4.5}]";
+
+        IEnumerable<Weapon> expectedWeapons = new List<Weapon>
+        {
+            new("Hand Axe", 3.5),
+            new("Forked Hatchet", 2.5),
+            new("Battle Axe", 4.5)
+        };
+
+        var mockHttpClient = new HttpClient(new MockHttpMessageHandler(response));
+        var mockOptions = Substitute.For<IOptions<RepositoryConfiguration>>();
+
+        mockOptions.Value.Returns(new RepositoryConfiguration
+        {
+            BaseAddress = "https://wbaldoumas.github.io/elden-ring-armor-optimizer"
+        });
+
+        var weaponRepository = new WeaponRepository(mockHttpClient, mockOptions);
+
+        // act
+        var weapons = await weaponRepository.GetAll();
+
+        // assert
+        weapons.Should().BeEquivalentTo(expectedWeapons);
+    }
+}
